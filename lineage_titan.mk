91819f39dfac1b3daa93c160c4b6ef326451e824
Aayush Gupta <aayushgupta219@gmail.com>
2021-03-20 06:43:47 +0000

    titan: Make sure proprietary binaries exists before compilation
    - Required to boot the OS
    
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Signed-off-by: althafvly <althafvly@gmail.com>
    Change-Id: Id478ed5b013488f1209f5944e08607180bc1a64a

--- lineage_titan.mk
+++ lineage_titan.mk
@@ -1,9 +1,34 @@
-$(call inherit-product, device/motorola/titan/full_titan.mk)
+#
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, device/motorola/titan/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := titan
 PRODUCT_RELEASE_NAME := titan
 PRODUCT_NAME := lineage_titan
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := titan
+PRODUCT_MANUFACTURER := motorola
+PRODUCT_SHIPPING_API_LEVEL := 19
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
