f294356446bed1c0d31370f726884c838fbcc034
Kevin F. Haggerty <haggertk@lineageos.org>
2021-04-09 08:18:37 -0600

    titan: Don't let builds complete without vendor tree
    * There is zero reason to ever build without blobs. We've even seen
      this situation with official builds from our servers. It's always
      better for a build to fail than it is for it to produce something
      that has no chance at working.
    
    Change-Id: I4968795670c91f691e9ecdc0e4af62e16ba3a93a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,4 +45,4 @@ BOARD_CACHEIMAGE_PARTITION_SIZE := 560070656
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 
 # inherit from the proprietary version
--include vendor/motorola/titan/BoardConfigVendor.mk
+include vendor/motorola/titan/BoardConfigVendor.mk
